8546e24e2520537ce922c55adbdbebee7eb7a868
Okoli Esther <okoliesther123@gmail.com>
2021-05-21 15:58:11 +0200

    Add files via upload

---
+++ checkYuGiOh.js
@@ -0,0 +1,33 @@
+function checkYuGiOh(n) {
+
+    let arr = [];
+
+    if (isNaN(n)) {
+        console.log(invalid parameter: $ { n });
+    } else {
+        for (let i = 1; i <= n; i++) {
+            if (i % 5 == 0 && i % 3 == 0 && i % 2 == 0) {
+                arr.push("yu-gi-oh");
+
+            } else if (i % 5 == 0 && i % 3 == 0) {
+                arr.push("gi-oh");
+            } else if (i % 5 == 0 && i % 2 == 0) {
+                arr.push("yu-oh");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                arr.push("yu-gi");
+            } else if (i % 2 == 0) {
+                arr.push("yu");
+            } else if (i % 3 == 0) {
+                arr.push("gi");
+            } else if (i % 5 == 0) {
+                arr.push("oh");
+            } else if (i % 2 == 0) {
+                arr.push("yu");
+            } else {
+                arr.push(i);
+            }
+        }
+    }
+    console.log(arr);
+}
+checkYuGiOh(5);
---
+++ convertFahrToCelsius.js
@@ -0,0 +1,14 @@
+function convertFahrToCelsius(F) {
+    let cTemp = [];
+}
+
+var F = window.prompt("Enter your number: ");
+
+if (isNaN(F)) {
+    console.log($ { F }
+        is not a valid number but a / an $ { F }.);
+} else {
+    cTemp = (F - 32) * 5 / 9;
+    //console.log(cTemp);
+    console.log(cTemp.toFixed(4));
+}
